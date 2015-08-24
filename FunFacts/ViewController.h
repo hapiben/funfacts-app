14ef0da6fa9118ab49d15642b0fffa7474a71a01
Benedict Aluan <benedict.aluan@gmail.com>
2015-08-25 09:06:52 +1200

    Initial Commit

---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Benedict Aluan on 25/08/15.
+//  Copyright (c) 2015 Pencil Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
